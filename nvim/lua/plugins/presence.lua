f59ae7d286c5d6e272c1f8febfc1729c0f1b8cae
unknown <tomasaltero@hotmail.com>
2024-09-29 16:42:09 -0300

    cambios

--- nvim/lua/plugins/presence.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"andweeb/presence.nvim",
-	opts = {
-		auto_update = true,
-		neovim_image_text = "The best text editor",
-		main_image = "file",
-		client_id = "793271441293967371",
-	},
-}
